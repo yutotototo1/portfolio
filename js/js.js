66d64ed40981ed76de4d82cabef92dfd5ecf0112
yutotototo1 <yutotototo1@gmail.com>
2024-08-06 20:00:44 +0900

    hover

--- js/js.js
+++ js/js.js
@@ -1,17 +1,25 @@
+
+
+   // 動きのきっかけの起点となるアニメーションの名前を定義
 function fadeAnime(){
 
-    // ふわっ
-    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-      var elemPos = $(this).offset().top-50;//要素より、50px上の
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight){
-      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-      }
-      });
-  }
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-    $(window).scroll(function (){
-      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-10;//要素より、10px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }
     });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });
