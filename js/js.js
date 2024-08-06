ba89f9bf19623369dc7977f49c759ab66f1df1dc
yutotototo1 <yutotototo1@gmail.com>
2024-08-06 10:20:28 +0900

    t

---
+++ js/js.js
@@ -0,0 +1,17 @@
+function fadeAnime(){
+
+    // ふわっ
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }
+      });
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });
